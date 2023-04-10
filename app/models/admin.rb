ccdc0f4cf65326e828c3b748b5ede69ab0052da9
root <root@localhost>
2023-04-10 08:29:02 +0000

    Updates wording on submit button for members page, implements differing choices between executives and leaders in members page (Double check if choices are what they are supposed to be)

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,8 @@
 class Admin < ApplicationRecord
+  def self.bypass_validations
+    yield
+  end
+  
   validates :email, presence: true, format: { with: /\A[\w+\-.]+@tamu\.edu\z/i, message: 'Email must be valid'}
   validates :phone, presence: true, format: { with: /\A(\+\d{1,2}\s?)?(\(\d{3}\)|\d{3})[\s.-]?\d{3}[\s.-]?\d{4}\z/, message: 'Phone must be valid'}
   validates :role, presence: true, format: { with: /\A(member|leader|executive)\z/i, message: 'Role must be valid'}
