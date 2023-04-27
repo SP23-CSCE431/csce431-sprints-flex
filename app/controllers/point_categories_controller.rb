f9887d1e8c2a4e7b190643a93afc0164150fa235
root <root@localhost>
2023-04-27 22:05:20 +0000

    Member accessing admin pages bugfix

--- app/controllers/point_categories_controller.rb
+++ app/controllers/point_categories_controller.rb
@@ -4,25 +4,40 @@ class PointCategoriesController < ApplicationController
   # GET /point_categories or /point_categories.json
   # Query all point category objects
   def index
+    if current_admin.role == "Member"
+      redirect_to(points_path)
+    end
     @point_categories = PointCategory.all
   end
 
   # GET /point_categories/1 or /point_categories/1.json
   def show
+    if current_admin.role == "Member"
+      redirect_to(points_path)
+    end
   end
 
   # GET /point_categories/new
   def new
+    if current_admin.role == "Member"
+      redirect_to(points_path)
+    end
     @point_category = PointCategory.new
   end
 
   # GET /point_categories/1/edit
   def edit
+    if current_admin.role == "Member"
+      redirect_to(points_path)
+    end
   end
 
   # POST /point_categories or /point_categories.json
   # Create a new point category object
   def create
+    if current_admin.role == "Member"
+      redirect_to(points_path)
+    end
     @point_category = PointCategory.new(point_category_params)
 
     respond_to do |format|
@@ -39,6 +54,9 @@ class PointCategoriesController < ApplicationController
   # PATCH/PUT /point_categories/1 or /point_categories/1.json
   # Update a point category
   def update
+    if current_admin.role == "Member"
+      redirect_to(points_path)
+    end
     respond_to do |format|
       if @point_category.update(point_category_params)
         format.html { redirect_to(point_category_url(@point_category), notice: "Point category was successfully updated.") }
@@ -53,6 +71,9 @@ class PointCategoriesController < ApplicationController
   # DELETE /point_categories/1 or /point_categories/1.json
   # Delete a point category
   def destroy
+    if current_admin.role == "Member"
+      redirect_to(points_path)
+    end
     @point_category.destroy
 
     respond_to do |format|
--- app/controllers/point_reviews_controller.rb
+++ app/controllers/point_reviews_controller.rb
@@ -1,17 +1,26 @@
 class PointReviewsController < ApplicationController
   def index
+    if current_admin.role == "Member"
+      redirect_to(points_path)
+    end
     @user_id = current_admin.id
       @pending_points = Point.paginate(page: params[:page], per_page: 10).where(is_approved: nil)
       @pending_points_count = Point.where(is_approved: nil).count
   end
 
   def approve
+    if current_admin.role == "Member"
+      redirect_to(points_path)
+    end
     pending_point = Point.find(params[:point_id])
       pending_point.update(is_approved: true)
       redirect_to(point_reviews_path)
   end
 
   def deny
+    if current_admin.role == "Member"
+      redirect_to(points_path)
+    end
     pending_point = Point.find(params[:point_id])
       pending_point.update(is_approved: false)
       redirect_to(point_reviews_path)
